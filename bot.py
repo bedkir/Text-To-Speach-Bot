11f57c092d07469db9cf6ecf7997cae2411eb094
bedkir <61096456+bedkir@users.noreply.github.com>
2020-02-21 22:27:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
 from datetime import datetime
 import time
-import gunicorn
+from gtts import gTTS
 bot = telebot.TeleBot('1009814377:AAHUvKXpCfiuxlwZ0lJ9U7T3ZhjPdjLUIrs')
 now = datetime.now()
 #functions
@@ -14,13 +14,16 @@ def time_now(message):
 @bot.message_handler(commands = ['date'])
 def date_today(message):
     bot.send_message(message.chat.id, now.strftime("%d-%m-%Y"))
+@bot.message_handler(content_types = ['text'])
+def speaching(message):
+    tts = gTTS(message.text, lang='ru')
+    tts.save('rock.mp3')
+    voice = open('rock.mp3', 'rb')
+    bot.send_voice(message.chat.id, voice)
 @bot.message_handler(commands = ['dayofweek'])
 def day_of_week(message):
     bot.send_message(message.chat.id, now.strftime('%A'))
 @bot.message_handler(commands = ['cat'])
 def hint(message):
     bot.send_photo(message.chat.id, 'AgACAgIAAxkBAAP1Xkhc5pfWjBvVIHjXSnHME9NuwVgAAoCuMRu2U0BKHT45zetIOKd9Y8sOAAQBAAMCAAN5AAMRTgIAARgE')
-@bot.message_handler(content_types = ['photo'])
-def photo_id(message):
-    print(message)
 bot.polling()
